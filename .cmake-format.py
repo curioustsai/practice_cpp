fb0a41e14cadc21bea295d102bf4f543a72df8cc
curioustsai <richer30@gmail.com>
2022-04-27 15:58:15 +0800

    update: cmake-format

---
+++ .cmake-format.py
@@ -0,0 +1,14 @@
+with section('format'):
+  # How many spaces to tab for indent
+  tab_size = 4
+
+  # If a statement is wrapped to more than one line, than dangle the closing
+  # # parenthesis on it's own line
+  dangle_parens = True
+
+  # Do not sort argument list
+  autosort = False
+
+with section('markup'):
+  # no comment markup parsing and reflow
+  enable_markup = False
