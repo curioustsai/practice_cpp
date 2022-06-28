455497959d5b56f21ddd1164a007c417ee58dfe7
ui-richard <richard.tsai@ui.com>
2022-06-28 08:31:26 +0800

    update 3 sum

--- code/leetcode/Array/15.3Sum.cpp
+++ code/leetcode/Array/15.3Sum.cpp
@@ -41,14 +41,46 @@ public:
 
         sort(nums.begin(), nums.end());
 
-        // TODO remove duplicate triplet
         for (int i = 0; i < nums.size() - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             for (int j = i + 1; j < nums.size() - 1; j++) {
-                if (nums[i] == nums[j]) continue;
                 int residue = -(nums[i] + nums[j]);
 
                 if (binary_search(nums, j + 1, nums.size() - 1, residue) != -1) {
                     res.push_back({nums[i], nums[j], residue});
+                }
+            }
+        }
+        return res;
+    }
+
+    // reference opt
+    vector<vector<int>> threeSum_opt2(vector<int>& nums) {
+        vector<vector<int>> res;
+        if (nums.size() < 3) {
+            res.push_back({});
+            return res;
+        }
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1, right = nums.size() - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    // when sum == 0
+                    res.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+
+                    while ((nums[left] == nums[left - 1]) && (left < right))
+                        left++; //move to next element
                 }
             }
         }
@@ -78,16 +110,16 @@ TEST(ThreeSum, Test1) {
     vector<vector<int>> ans_swap = {{-1, -1, 2}, {-1, 0, 1}};
 
     // vector<vector<int>> res = sol.threeSum(nums);
-    vector<vector<int>> res = sol.threeSum_opt(nums);
+    vector<vector<int>> res = sol.threeSum_opt2(nums);
 
     for (vector<int> v : res) {
         for (int i : v) cout << i << " ";
         cout << endl;
     }
-    // bool eq = equal(ans.begin(), ans.end(), res.begin());
-    // eq |= equal(ans_swap.begin(), ans_swap.end(), res.begin());
+    bool eq = equal(ans.begin(), ans.end(), res.begin());
+    eq |= equal(ans_swap.begin(), ans_swap.end(), res.begin());
 
-    // ASSERT_EQ(eq, true);
+    ASSERT_EQ(eq, true);
 }
 
 TEST(ThreeSum, Test2) {
