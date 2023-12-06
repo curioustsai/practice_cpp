aa9b3b000b4d4934665cc0e913a36d4188a714f1
curioustsai <richer30@gmail.com>
2023-12-06 14:52:17 +0800

    (update) backtracking

---
+++ code/leetcode/Backtracking/131.PalindromePartition.cpp
@@ -0,0 +1,62 @@
+/*
+ * Given a string s, partition s such that every substring of the partition is a palindrome.
+ * Return all possible palindrome partitioning of s.
+ * https://leetcode.com/problems/palindrome-partitioning/
+*/
+
+#include <gtest/gtest.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<string> curr;
+        vector<vector<string>> results;
+
+        dfs(s, 0, curr, results);
+        return results;
+    }
+
+    void dfs(string s, int start, vector<string>& curr, vector<vector<string>>& results){
+        if (start == s.size()) {
+            results.push_back(curr);
+            return;
+        }
+
+        for (int i=start; i < s.size(); i++) {
+            if (isPalindrome(s, start, i)) {
+                string ss = s.substr(start, i - start + 1);
+                curr.push_back(ss);
+                dfs(s, i + 1, curr, results);
+                curr.pop_back();
+            }
+        }
+    }
+
+    bool isPalindrome(string s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            right--;
+            left++;
+        }
+        return true;
+    }
+};
+
+TEST(PalindromePartition, Example1) {
+    Solution sol;
+    string s = "aab";
+    vector<vector<string>> ans = {{"a", "a", "b"}, {"aa", "b"}};
+
+    ASSERT_EQ(sol.partition(s), ans);
+}
+
+TEST(PalindromePartition, Example2) {
+    Solution sol;
+    string s = "a";
+    vector<vector<string>> ans = {{"a"}};
+
+    ASSERT_EQ(sol.partition(s), ans);
+}
---
+++ code/leetcode/Backtracking/51.N-Queens.cpp
@@ -0,0 +1,69 @@
+/*
+ * The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+* https://leetcode.com/problems/n-queens/
+*
+*/
+
+#include <gtest/gtest.h>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+        vector<vector<string>> result;
+        backtrack(n, 0, result, board);
+
+        return result;
+    }
+
+private:
+    set<int> cols;
+    set<int> negDiag;
+    set<int> posDiag;
+
+    void backtrack(int n, int row, vector<vector<string>>& result, vector<string>& board) {
+        if (row == n) {
+            result.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (cols.find(col) != cols.end() ||
+                negDiag.find(row-col) != negDiag.end() ||
+                posDiag.find(row+col) != posDiag.end())
+                continue;
+
+            cols.insert(col);
+            negDiag.insert(row - col);
+            posDiag.insert(row + col);
+            board[row][col] = 'Q';
+
+            backtrack(n, row + 1, result, board);
+
+            cols.erase(col);
+            negDiag.erase(row - col);
+            posDiag.erase(row + col);
+            board[row][col] = '.';
+        }
+    }
+};
+
+TEST(NQueens, Example1) {
+    Solution sol;
+    int n = 4;
+    vector<vector<string>> ans = {{".Q..","...Q","Q...","..Q."},{"..Q.","Q...","...Q",".Q.."}};
+
+    ASSERT_EQ(sol.solveNQueens(n), ans);
+}
+
+TEST(NQueens, Example2) {
+    Solution sol;
+    int n = 1;
+    vector<vector<string>> ans = {{"Q"}};
+
+    ASSERT_EQ(sol.solveNQueens(n), ans);
+}
