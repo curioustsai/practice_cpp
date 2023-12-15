2e91df23b75997e16fe1e9e1038254f444536681
curioustsai <richer30@gmail.com>
2023-12-15 17:32:36 +0800

    (add) leetcode practices

---
+++ code/leetcode/Misc/355.DesignTweeter.cpp
@@ -0,0 +1,57 @@
+/*
+*
+* https://leetcode.com/problems/design-twitter/
+*/
+
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+class Twitter {
+public:
+    Twitter() {
+        
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        posts.push_back({userId, tweetId});
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        int count = 0;
+        vector<int> res;
+        for (int i = posts.size() - 1; i >= 0; i--) {
+            if (count == 10) break;
+            if (map[userId].count(posts[i].first) || posts[i].first == userId) {
+                count++;
+                res.push_back(posts[i].second);
+            }
+        }
+        return res;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        map[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        map[followerId].erase(followeeId);
+    }
+private:
+    // followerID, a bunch of followeeID
+    unordered_map<int, unordered_set<int>> map;
+    // {userID, postID}
+    vector<pair<int, int>> posts;
+
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
